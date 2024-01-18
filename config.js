b0624814e031588ac21db6e210780ec094a6f69a
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-18 13:17:48 +0100

    〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', true], //〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕
-["525526591934"], 
-["5492996111177"]]
+["528995015765", '〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕', true], //〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕
+["528995015765"], 
+["528995015765"]]
 
 global.mods = []
 global.prems = []
@@ -104,11 +104,11 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1], 
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1],  
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1],
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1],
-["51902360712", '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕', 1]]
+["528995015765", '〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕', 1], 
+["528995015765", '〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕', 1],  
+["528995015765", '〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕', 1],
+["528995015765", '〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕', 1],
+["528995015765", '〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -116,8 +116,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = "〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕"
-global.author = "〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕"
+global.packname = "〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕"
+global.author = "〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > CUENTAS E INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -125,7 +125,7 @@ global.author = "〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕"
 // [EN] > ACCOUNTS AND BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
-global.gt = "〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕"
+global.gt = "〔 𝗠𝗶𝗱𝗮𝗿𝗶 𝗕𝗼𝘁 〕"
 
 global.yt = "https://www.instagram.com/optimus.ventas.bot"
 global.yt2 = "https://www.instagram.com/optimus.ventas.bot"
@@ -140,7 +140,7 @@ global.asistencia = 'https://wa.me/51902360711' //Contacto
 global.nna = 'https://chat.whatsapp.com/FlSQ9PkM8lB5YPRv73ZbQ3' // CANAL UPDATE
 global.nn2 = 'https://www.instagram.com/optimus.ventas.bot' // CANAL GataBot
 global.nna2 = 'https://www.instagram.com/optimus.ventas.bot' // Help
-global.nn = 'https://chat.whatsapp.com/FlSQ9PkM8lB5YPRv73ZbQ3' // Grupo 1
+global.nn = 'https://www.instagram.com/optimus.ventas.bot' // Grupo 1
 global.nnn = 'https://www.instagram.com/optimus.ventas.bot' // Grupo 2
 global.nnnt = 'https://www.instagram.com/optimus.ventas.bot' // Grupo 3
 global.nnntt = 'https://www.instagram.com/optimus.ventas.bot' // Grupo 4
